6fd1548ffcade4947826bec8f80950dc9771f409
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-19 20:03:06 +0530

    Added : Play till winning

--- SnakeNLadder/src/com/bridgelabz/Assignment4/SnakeLadder/SnakeNLadder.java
+++ SnakeNLadder/src/com/bridgelabz/Assignment4/SnakeLadder/SnakeNLadder.java
@@ -1,33 +1,40 @@
 package com.bridgelabz.Assignment4.SnakeLadder;
 
 /**
- * @author: Sadanand Pandey
- *  Added play options
+ * @author atik Continue till winning position
  */
 public class SnakeNLadder {
 	private static final int noPlay = 0;
 	private static final int ladder = 1;
 	private static final int snake = 2;
+	private static final int maxPosition = 100;
+	private static final int minPosition = 0;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to SNAKES AND ladderS GAME!!");
-		int startPositionA = 0;
-		int dice = (int) (Math.floor(Math.random() * 6) + 1);
-		System.out.println(dice);
-		int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
-		System.out.println(checkPlay);
+		System.out.println("Welcome to SNAKES AND LADDERS GAME!!");
+		int positionA = 0;
 
-		switch (checkPlay) {
-		case noPlay:
-			break;
-		case ladder:
-			startPositionA += dice;
-			break;
-		case snake:
-			startPositionA -= dice;
-			break;
+		while (positionA < maxPosition) {
+			int dice = (int) (Math.floor(Math.random() * 6) + 1);
+			System.out.println("dice : " + dice);
+			int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("checkplay : " + checkPlay);
+
+			switch (checkPlay) {
+			case noPlay:
+				break;
+
+			case ladder:
+				positionA += dice;
+				break;
+
+			case snake:
+				positionA -= dice;
+				if (positionA < minPosition)
+					positionA = 0;
+				break;
+			}
+			System.out.println("end : " + positionA);
 		}
-		System.out.println(startPositionA);
 	}
-
 }
