6a91bd06fc317e8eeb11f1c214eba5c17ac41455
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-19 19:44:42 +0530

    Added: added play option

--- SnakeNLadder/src/com/bridgelabz/Assignment4/SnakeLadder/SnakeNLadder.java
+++ SnakeNLadder/src/com/bridgelabz/Assignment4/SnakeLadder/SnakeNLadder.java
@@ -1,13 +1,33 @@
 package com.bridgelabz.Assignment4.SnakeLadder;
+
 /**
- * @author atik
- * Generate randome dice number
+ * @author: Sadanand Pandey
+ *  Added play options
  */
 public class SnakeNLadder {
+	private static final int noPlay = 0;
+	private static final int ladder = 1;
+	private static final int snake = 2;
+
 	public static void main(String[] args) {
-			System.out.println("Welcome to SNAKES AND LADDERS GAME!!");
-			
-			int startPositionA = 0;
-			int dice = (int) (Math.floor(Math.random() * 6) + 1);
+		System.out.println("Welcome to SNAKES AND ladderS GAME!!");
+		int startPositionA = 0;
+		int dice = (int) (Math.floor(Math.random() * 6) + 1);
+		System.out.println(dice);
+		int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
+		System.out.println(checkPlay);
+
+		switch (checkPlay) {
+		case noPlay:
+			break;
+		case ladder:
+			startPositionA += dice;
+			break;
+		case snake:
+			startPositionA -= dice;
+			break;
 		}
+		System.out.println(startPositionA);
 	}
+
+}
