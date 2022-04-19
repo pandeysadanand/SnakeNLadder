6021c458ffee8d1998aa45064d4ebdee738772b1
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-19 19:34:51 +0530

    Added : wecome file

---
+++ SnakeNLadder/src/com/bridgelabz/Assignment4/SnakeLadder/SnakeNLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Assignment4.SnakeLadder;
+
+/**
+ * @author Sadanand Pandey Welcome message and start position is 0
+ */
+public class SnakeNLadder {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to SNAKES AND LADDERS GAME!!");
+		System.out.println("------------------------------");
+		int startPosition = 0;
+	}
+}
